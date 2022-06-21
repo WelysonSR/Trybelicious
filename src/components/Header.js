805f78e8642b5fa6809147e6a14009890a9019fa
Welyson <welysonsr@gmail.com>
2022-06-21 17:45:06 -0300

    req: 14

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,37 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchIngredient, fetchName, fetchFirstLetter } from '../redux/actions';
 import './Header.css';
 
 function Header({ img1, title, img2 }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [search, setSearch] = useState('');
+  const [choice, setChoice] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const NUMBER = 1;
+    switch (choice) {
+    case 'Ingredient':
+      dispatch(fetchIngredient(search));
+      break;
+    case 'Name':
+      dispatch(fetchName(search));
+      break;
+    case 'First Letter':
+      if (search.length > NUMBER) {
+        global.alert('Your search must have only 1 (one) character');
+      } else {
+        dispatch(fetchFirstLetter(search));
+      }
+      break;
+    default:
+      break;
+    }
+  };
 
   return (
     <header>
@@ -23,44 +50,58 @@ function Header({ img1, title, img2 }) {
       </section>
       {
         isOpen && (
-          <form className="open">
+          <form className="open" onSubmit={ handleSubmit }>
             <input
               data-testid="search-input"
               className="search-input"
               type="text"
+              name="search"
+              value={ search }
+              onChange={ ({ target }) => setSearch(target.value) }
               placeholder="Search"
             />
             <section className="radio-input">
               <label htmlFor="Ingredient">
                 <input
+                  data-testid="ingredient-search-radio"
                   type="radio"
                   name="search-type"
                   value="Ingredient"
                   id="Ingredient"
+                  onChange={ ({ target }) => setChoice(target.value) }
                 />
                 Ingredient
               </label>
               <label htmlFor="Name">
                 <input
+                  data-testid="name-search-radio"
                   type="radio"
                   name="search-type"
                   value="Name"
                   id="Name"
+                  onChange={ ({ target }) => setChoice(target.value) }
                 />
                 Name
               </label>
               <label htmlFor="First-Letter">
                 <input
+                  data-testid="first-letter-search-radio"
                   type="radio"
                   name="search-type"
                   value="First Letter"
                   id="First-Letter"
+                  onChange={ ({ target }) => setChoice(target.value) }
                 />
                 First Letter
               </label>
             </section>
 
-            <button type="submit" className="submit-btn" variant="primary">
+            <button
+              data-testid="exec-search-btn"
+              type="submit"
+              className="submit-btn"
+              variant="primary"
+            >
               Search
             </button>
           </form>
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,30 @@
 export const EMAIL_REDUX = 'EMAIL-REDUX';
+export const SAVE_INGREDIENT = 'SAVE_INGREDIENT';
 
 export const saveEmail = (action) => ({
   type: EMAIL_REDUX,
   action,
 });
+
+export const saveIngredient = (action) => ({
+  type: SAVE_INGREDIENT,
+  action,
+});
+
+export const fetchIngredient = (param) => async (dispatch) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${param}`);
+  const { meals } = await response.json();
+  dispatch(saveIngredient(meals));
+};
+
+export const fetchName = (param) => async (dispatch) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${param}`);
+  const data = await response.json();
+  dispatch(saveIngredient(data));
+};
+
+export const fetchFirstLetter = (param) => async (dispatch) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${param}`);
+  const data = await response.json();
+  dispatch(saveIngredient(data));
+};
