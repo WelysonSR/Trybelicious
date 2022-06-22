28e7a1274179b28d4f0001123a021fcd0c9a6c16
Welyson <welysonsr@gmail.com>
2022-06-22 17:33:44 -0300

    [Group 23 ] Requisito 16 á 18

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,20 +1,48 @@
-import React, { useEffect } from 'react';
-import { useStore } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Card from '../components/Card';
 import Header from '../components/Header';
 import Profile from '../images/profileIcon.svg';
 import Search from '../images/searchIcon.svg';
+import './All.css';
 
 function Foods() {
-  const store = useStore().getState();
+  const [meal, setMeals] = useState();
+  const meals = useSelector((state) => state.foods.meals);
+  const history = useHistory();
 
   useEffect(() => {
-    console.log(store);
-  });
+    if (!meals) {
+      return global.alert('Sorry, we haven\'t found any recipes for these filters.');
+    }
+
+    const NUMBER = 1;
+    if (meals.length === NUMBER) {
+      history.push(`/foods/${meals[0].idMeal}`);
+    } else {
+      const AMOUNT = 11;
+      const newMeals = meals.filter((_meal, i) => (i <= AMOUNT));
+      setMeals(newMeals);
+    }
+  }, [meals, history]);
 
   return (
-    <div>
+    <>
       <Header img1={ Profile } title="Foods" img2={ Search } />
-    </div>
+      <main className="cardAll">
+        {
+          meal && meal.map((card, i) => (
+            <Card
+              key={ card.idMeal }
+              img={ card.strMealThumb }
+              title={ card.strMeal }
+              index={ i }
+            />
+          ))
+        }
+      </main>
+    </>
   );
 }
 
