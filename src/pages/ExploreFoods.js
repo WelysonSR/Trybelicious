67f11b128711a595c43721330ec8249b637b433d
Welyson <welysonsr@gmail.com>
2022-06-27 17:55:23 -0300

    [Group 23] Requisito 70 á 74

--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Profile from '../images/profileIcon.svg';
 
 function ExploreFoods() {
+  const history = useHistory();
+
+  const randomFood = async () => {
+    const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    const { meals } = await (await fetch(url)).json();
+    history.push(`/foods/${meals[0].idMeal}`);
+  };
+
   return (
     <div>
       <Header img1={ Profile } title="Explore Foods" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ randomFood }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
